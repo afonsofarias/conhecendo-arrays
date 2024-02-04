091ae846546ee93d923e3b5985e2698ec5604ca7
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-04 15:20:20 -0300

    alterando com splice

---
+++ lista-estudantes.js
@@ -0,0 +1,7 @@
+const listaEstudantes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
+
+console.log(listaEstudantes);
+
+listaEstudantes.splice(1,2, 'Afonso');
+
+console.log(listaEstudantes);
